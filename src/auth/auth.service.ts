d4076929ba868e699e19262d5e303171ed5386bc
oustani-anas <anas.migos20@gmail.com>
2024-04-06 11:59:25 +0000

    let see

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,14 +12,14 @@ export class AuthService {
 
   async login(username: string, password: string): Promise<string | null> {
     // Call your user service or repository to fetch user data
-    /*const user = await this.userService.findByUsername(username);
+    const user = await this.userService.findByUsername(username);
 
     // Check if user exists and password matches
     if (user && user.password === password) {
       // Generate JWT token with user payload
       const payload = { username: user.username, sub: user.id }; // You can include any user data here
       return this.jwtService.sign(payload);
-    }*/
+    }
 
     return null; // Return null if authentication fails
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,16 @@
 
 import { Injectable } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
 export class UserService {
+    constructor(private readonly prisma: PrismaClient){}
 
+    async findByUsername(username: string) {
+        return this.prisma.user.findUnique({
+            where: {
+                username: username,
+            },
+        });
+    } 
 }
